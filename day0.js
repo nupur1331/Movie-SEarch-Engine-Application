24cca9ac81c1f5dc31f566c6963bedf50f9b1bc5
nupur1331 <66845631+nupur1331@users.noreply.github.com>
2021-02-28 13:08:44 -0800

    Update day0.js

--- day0.js
+++ day0.js
@@ -3,16 +3,23 @@ const request = require('request')
 const name = "nupur"
 console.log(colors.green(name))
 const express = require('express')
-
 //ask express.js to look for a folder called views - inbuild functionailty of express.js
+const dotenv = require('dotenv')
+dotenv.config()
 const app = express()
 app.set("view engine","ejs")
+
 app.get("/",(req,res)=>{
-    res.render("homepage.ejs")
+    res.render("homepage")
+})
+app.get("/aboutme",(req,res)=>{
+    res.render("aboutme")
 })
 app.get("/result",(req, res) => {
-        console.log(req.query.movieName)
-        const url = `http://www.omdbapi.com/?apikey=621504fb&s=${req.query.movieName}`
+        //console.log(req.query.movieName)
+        console.log(process.env.API_KEY)
+        const url = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${req.query.movieName}`
+
         request(url, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 //parsing json into valid javascript object
@@ -26,12 +33,26 @@ app.get("/result",(req, res) => {
             }
         })
     })
+    app.get("/result/:id",(req,res)=>{
+        console.log(req.params.id)
+        const url = `http://www.omdbapi.com/?apikey=621504fb&i=${req.params.id}`
+        request(url,function(error,response,body){
+            if(!error && response.statusCode==200)
+            {
+                const data = JSON.parse(body)
+                console.log(data)
+                res.render("info",{movie:data})
+            }
+            else{
+                res.send("uh oh ! error!!")
+            }
+        })
+    })
 app.get("*",(req,res)=>{
     res.send("error page")
 })
-app.listen(3000,()=>{
-     console.log("server has started  ")
-
+app.listen(process.env.port,()=>{
+     console.log(`server has started ${process.env.port}`)
 })
 //port-3000,5000,8000
 //app.listen(port no , call back function(arrow funnction))
